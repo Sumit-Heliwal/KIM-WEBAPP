02c6520b2c8d148f3d5743e01afb487827bdc1b4
Sumit Heliwal <47483109+Sumit-Heliwal@users.noreply.github.com>
2023-01-02 14:10:09 +0530

    02-01-2023 Data Insertion and Hr

--- HR_Department/admin.py
+++ HR_Department/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Employee_Data, Leave_Data
+from .models import Employee_Data, Leave_Data, Reporting_Officer
 
 admin.site.register(Employee_Data)
 admin.site.register(Leave_Data)
+admin.site.register(Reporting_Officer)
--- HR_Department/models.py
+++ HR_Department/models.py
@@ -13,10 +13,18 @@ Blood_group = (
     ( "AB -ve", "AB -ve"),
     
 )
-
-class Employee_Data(models.Model):
-    Emp_Code = models.CharField(max_length= 12, primary_key=True)
+class Reporting_Officer(models.Model):
+    Emp_Code = models.CharField(max_length= 4, primary_key=True)
     Employee_Name = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+         return (self.Emp_Code + ", " + self.Employee_Name )
+    
+     
+class Employee_Data(models.Model):
+    Emp_Code = models.ForeignKey(Reporting_Officer, on_delete=models.CASCADE)
+    Emp_type = models.TextChoices('Emp_type', 'Teaching Non_Teaching')
+    Emp_Type = models.CharField(max_length=15, choices=Emp_type.choices, default="Non_Teaching")
     Designation = models.CharField(max_length=25)
     Address_1 = models.CharField(max_length=300)
     Address_2 = models.CharField(max_length=300)
@@ -28,14 +36,16 @@ class Employee_Data(models.Model):
     Date_of_Joining = models.DateField()
     Date_of_Confirmation = models.DateField()
     Blood_Group =models.CharField(max_length=6, choices=Blood_group, default="A +ve")
-
+    Reporting_Authority = models.ForeignKey(Reporting_Officer,related_name='Reporting_Authority', on_delete=models.CASCADE, default='1111')
+    
+    
 
 
 class Leave_Data(models.Model):
-    Emp_Code = models.ForeignKey(Employee_Data, on_delete=models.CASCADE)
+    Emp_Code = models.ForeignKey(Reporting_Officer, on_delete=models.CASCADE)
     Leave_Type = models.CharField(max_length=25)
     From_Date = models.DateField()
     To_Date = models.DateField()
-    Recommended_Authority = models.ForeignKey(Employee_Data, related_name='RECOMENDED_AYTHORITY' ,on_delete=models.CASCADE)
-    Approved_By = models.ForeignKey(Employee_Data, related_name='APPROVED_BY', on_delete=models.CASCADE)
+    Recommended_Authority = models.ForeignKey(Reporting_Officer, related_name='RECOMENDED_AYTHORITY' ,on_delete=models.CASCADE)
+    Approved_By = models.ForeignKey(Reporting_Officer, related_name='APPROVED_BY', on_delete=models.CASCADE)
     Reason_for_Leave = models.CharField(max_length=300)
